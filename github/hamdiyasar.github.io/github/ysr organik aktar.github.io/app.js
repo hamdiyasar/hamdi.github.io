6e7394562c5a15d6a692596a3b89d42c551868e3
hamdiyasar <hamdiysr@gmail.com>
2024-07-05 01:18:09 +0300

    Add files via upload

---
+++ github/hamdiyasar.github.io/github/ysr organik aktar.github.io/app.js
@@ -0,0 +1,12 @@
+function showMoreInfo(product) {
+    const moreInfoId = product + '-more-info';
+    const moreInfoElement = document.getElementById(moreInfoId);
+
+    if (moreInfoElement.classList.contains('hidden')) {
+        moreInfoElement.classList.remove('hidden');
+    } else {
+        moreInfoElement.classList.add('hidden');
+    }
+}
+
+console.log("Organik Ürünler web sitesi yüklendi.");f
